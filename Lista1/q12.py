7a363546bab1c36808e16a3db258e9b05218ffcb
Givanaldo Rocha <givanaldo@gmail.com>
2014-06-20 22:02:15 -0300

    Questões da lista 1

--- Lista1/q12.py
+++ Lista1/q12.py
@@ -2,4 +2,13 @@
 # Professor: Givanaldo Rocha
 # Lista 1 - Questão 12
 
+# entrada da largura e do comprimentos da sala
+largura = float(input('Largura da sala em metros: '))
+comprimento = float(input('Comprimento da sala em metros: '))
 
+# cálculo da área da sala em metros quadrados
+area = largura * comprimento
+
+# cálculo da potência necessária, sabendo que 1 metro quadrado necessita de 18 W
+potencia = area * 18
+print("Para iliminar")
