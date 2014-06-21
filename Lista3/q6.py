e753f4da19def983eb4138195842f0b581028823
Givanaldo Rocha <givanaldo@gmail.com>
2014-06-21 17:51:54 -0300

    Questão 6

--- Lista3/q6.py
+++ Lista3/q6.py
@@ -1,3 +1,16 @@
 # Disciplina: Programação Estrutura e Orientada a Objetos
 # Professor: Givanaldo Rocha
 # Lista 3 - Questão 6
+
+# entrada do número que deseja saber se é triangular
+num = int(input('Entre com o número: '))
+é_triangular = False
+
+for i in range(3, num+1):
+    if (i-2)*(i-1)*i == num:
+        print("O número %d é triangular, pois %d x %d x %d = %d" % (num, i-2, i-1, i, num))
+        é_triangular = True
+        break
+
+if é_triangular == False:
+    print('O número %d não é triangular.')
