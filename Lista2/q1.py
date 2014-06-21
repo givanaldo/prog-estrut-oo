8eb22a8c17f59cd9a2f8f68c4d4fcbef9d10caff
Givanaldo Rocha <givanaldo@gmail.com>
2014-06-20 23:02:56 -0300

    Questão 1

--- Lista2/q1.py
+++ Lista2/q1.py
@@ -2,17 +2,15 @@
 # Professor: Givanaldo Rocha
 # Lista 2 - Questão 1
 
-
+# entrada dos lados do triângulo (a, b e c)
 a = int(input('Lado a: '))
 b = int(input('Lado b: '))
 c = int(input('Lado c: '))
 if (a > b + c) or (b > a + c) or (c > a + b):
-  print ('N�o pode ser um tri�ngulo')
-  print ('Um dos lados � maior que a soma dos outros')
+  print('Não pode ser um triângulo, pois um dos lados é maior que a soma dos outros.')
 elif a == b == c:
-  print ('Equil�tero')
+  print ('O triângulo é Equilátero.')
 elif a == b or b == c or a == c:
-  print ('Is�sceles')
+  print ('O triângulo é Isósceles.')
 else:
-  print ('Escaleno')
-  
+  print ('O triângulo é Escaleno.')
