b93896dcd163552032dc05a5c36beba7b55676aa
Givanaldo Rocha <givanaldo@gmail.com>
2014-06-21 15:26:16 -0300

    Questão 2

---
+++ Lista3/q2.py
@@ -0,0 +1,15 @@
+# Disciplina: Programação Estrutura e Orientada a Objetos
+# Professor: Givanaldo Rocha
+# Lista 3 - Questão 2
+
+# entrada do usuário
+usuario = input('Usuário: ')
+
+# entrada da senha até que a mesma seja diferente do nome do usuário
+senha = usuario
+while senha == usuario:
+    senha = input('Senha: ')
+    if senha == usuario:
+        print('A senha não pode ser igual ao nome de usuário. Entre com a senha novamente!!!')
+
+print('Usuário e senha registrados com sucesso.')
