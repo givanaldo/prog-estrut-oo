10479a7fb10c8891a1740fe85b9a62180bbb157d
Givanaldo Rocha <givanaldo@gmail.com>
2014-06-21 18:20:07 -0300

    Questão 7

---
+++ Lista3/q7.py
@@ -0,0 +1,18 @@
+# Disciplina: Programação Estrutura e Orientada a Objetos
+# Professor: Givanaldo Rocha
+# Lista 3 - Questão 7
+
+# entrada do valor da conta e do valor pago, desprezando os centavos
+# Ou seja, as entradas são de números inteiros
+conta = int(input('Valor da conta: '))
+pagamento = int(input('Valor do pagamento: '))
+
+# Notas disponíveis: 50, 20, 10, 5, 2, 1
+troco = pagamento - conta
+print("Troco: R$ %d" % troco)
+notas = [50, 20, 10, 5, 2, 1]
+for nota in notas:
+    quantidade_notas = troco // nota
+    if quantidade_notas != 0:
+        print("   -- %d nota(s) de %d" % (quantidade_notas, nota))
+    troco = troco % nota
