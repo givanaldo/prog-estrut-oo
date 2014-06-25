0700a6c0abd0221fa0f3db9ef63f4906a71cf8a3
Givanaldo Rocha <givanaldo@gmail.com>
2014-06-25 11:32:22 -0300

    Questão 3

---
+++ Lista4/q3.py
@@ -0,0 +1,24 @@
+# Disciplina: Programação Estrutura e Orientada a Objetos
+# Professor: Givanaldo Rocha
+# Lista 3 - Questão 2
+
+# módulo para gerar números aleatórios
+import random
+
+# gera números aleatórios entre 1 e 100 e adiciona nas duas listas
+lista1 = []
+lista2 = []
+for i in range(10):
+    lista1.append(random.randint(1,100))
+    lista2.append(random.randint(1,100))
+
+print(lista1)
+print(lista2)
+
+# cria terceira lista com os elementos das outras duas listas alternados
+lista3 = []
+for x in range(10):
+    lista3.append(lista1[x])
+    lista3.append(lista2[x])
+
+print(lista3)
